40e9a3cb3001cec0b9a2864fc3838bad82fd4d3b
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2024-02-21 18:56:59 +0100

    feat: adds plan reconstruction to task

--- src/task/mod.rs
+++ src/task/mod.rs
@@ -7,8 +7,12 @@ pub mod r#type;
 use self::{
     action::Action, object::Object, predicate::Predicate, r#type::Type,
 };
-use crate::state::{Fact, State};
+use crate::{
+    state::{Fact, State},
+    successor_generation::{instantiate_actions, Operator},
+};
 
+pub type Plan<'a> = Vec<Operator<'a>>;
 pub type Goal = Vec<(Fact, bool)>;
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub struct Task {
@@ -33,5 +37,34 @@ impl Task {
         }
 
         objects
+    }
+    pub fn trace_path(&self, states: &Vec<State>) -> Plan {
+        let mut path = Vec::new();
+
+        for i in 0..states.len() - 1 {
+            let state = &states[i];
+            let operators = instantiate_actions(self, state);
+            let operator = operators
+                .into_iter()
+                .find(|o| state.apply(o.action, &o.args) == states[i + 1])
+                .unwrap();
+            path.push(operator);
+        }
+
+        path
+    }
+    pub fn export_plan(&self, plan: &Plan) -> String {
+        plan.iter()
+            .map(|o| {
+                format!(
+                    "({}{})\n",
+                    o.action.name,
+                    o.args
+                        .iter()
+                        .map(|i| format!(" {}", self.objects[*i].name))
+                        .collect::<String>()
+                )
+            })
+            .collect()
     }
 }
