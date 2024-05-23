8869ee6c1aa93358a35d9e71adc2e73a217962be
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2024-05-23 12:34:44 +0200

    perf: store facts as fact

--- src/state.rs
+++ src/state.rs
@@ -2,57 +2,62 @@ use std::collections::BTreeSet;
 
 use crate::task::{action::Action, Goal};
 
-#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+#[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Fact {
     pub predicate: usize,
     pub args: Vec<usize>,
 }
 
+impl Fact {
+    pub fn new(predicate: usize, args: Vec<usize>) -> Fact {
+        Self { predicate, args }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct State {
-    facts: Vec<BTreeSet<Vec<usize>>>,
+    facts: BTreeSet<Fact>,
 }
 
 impl State {
-    pub fn new(predicate_count: usize, facts: Vec<Fact>) -> Self {
-        let mut internal_facts =
-            vec![BTreeSet::<Vec<usize>>::new(); predicate_count];
-        for fact in facts.into_iter() {
-            internal_facts[fact.predicate].insert(fact.args);
-        }
+    pub fn new(_: usize, facts: Vec<Fact>) -> Self {
         State {
-            facts: internal_facts,
+            facts: facts.into_iter().collect(),
         }
     }
     #[inline(always)]
     pub fn has_nullary(&self, predicate: usize) -> bool {
-        self.facts[predicate].contains(&vec![])
+        self.facts.contains(&Fact::new(predicate, vec![]))
     }
     #[inline(always)]
     pub fn has_unary(&self, predicate: usize, arg: &usize) -> bool {
-        self.facts[predicate].contains(&vec![*arg])
+        self.facts.contains(&Fact::new(predicate, vec![*arg]))
     }
     #[inline(always)]
     pub fn has_nary(&self, predicate: usize, args: &Vec<usize>) -> bool {
-        self.facts[predicate].contains(args)
+        self.facts.contains(&Fact::new(predicate, args.to_owned()))
+    }
+    #[inline(always)]
+    pub fn has_fact(&self, fact: &Fact) -> bool {
+        self.facts.contains(fact)
     }
     pub fn apply(&self, action: &Action, args: &Vec<usize>) -> Self {
         let mut state = self.clone();
         for atom in action.effect.iter() {
             let args = atom.map_args(args);
+            let fact = Fact::new(atom.predicate, args);
             match atom.value {
                 true => {
-                    state.facts[atom.predicate].insert(args.to_owned());
+                    state.facts.insert(fact);
                 }
                 false => {
-                    state.facts[atom.predicate].remove(&args);
+                    state.facts.remove(&fact);
                 }
             }
         }
         state
     }
     pub fn covers(&self, goal: &Goal) -> bool {
-        goal.iter()
-            .all(|(f, v)| self.has_nary(f.predicate, &f.args) == *v)
+        goal.iter().all(|(f, v)| self.has_fact(f) == *v)
     }
 }
